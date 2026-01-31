73ba06398e4ab5c2675fccbd5fff8d6b76d75522
Arus Rangra <arushrangra@gmail.com>
2026-01-31 22:31:36 +0530

    Refactor FirstClass to include star pattern printing

--- FirstClass.java
+++ FirstClass.java
@@ -1,205 +1,30 @@
-// class FirstClasss {
-//     public static void main(String args[]){
-//         int a = 10;
-//         int b = 14;
-//         //int sum = a+b;
-//        // System.out.println(sum);
-//       //  int diff = b -a;
-//       //  System.out.println(diff);
-//       int mul = a*b;
-//       System.out.println(mul);
 
-         
+import java.util.*;
+  public class FirstClass{
+    public static void main (String args[]){
+      for (int i =1;i<=4;i++){
+        System.out.println("****");
+      }
+    }
+  }
 
-//      }
-
-
-// import java.util.*;
-// class FirstClass {
-//   public static void main(String args[]){
-//     Scanner sc  = new Scanner(System.in);
-//     int a = sc.nextInt();
-//     int b = sc.nextInt();
-//     int sum = a+b;
-//     System.out.println(sum);
-//  }
-// } 
-
-// import java.util.*;
-// public class FirstClass{
-//   public static void main(String args []){
-//     Scanner sc = new Scanner(System.in);
-//     int age = sc.nextInt();
-    
-//     if (age>18){
-//       System.out.println("adult");
-//     }else{
-//       System.out.println("not adult");
-//     }
-
-//   }
-// }
-
-
-//  odd or even no
-
-// import java.util.*;
-// public class FirstClass{
-//   public static void main(String args[]){
-//   Scanner sc = new Scanner (System.in);
-//   int x = sc.nextInt();
-
-//   if(x%2==0){
-//     System.out.println("even no");
-//   }else{
-//     System.out.println("odd no");
-//   }
-//   }
-// }
-
-
-// import java.util.*;
-// public class FirstClass{
-//   public static void main(String args[]){
-//     Scanner sc = new Scanner(System.in);
-//     int a = sc.nextInt();
-//     int b = sc.nextInt();
-
-//     if(a==b){
-//       System.out.println("equl");
-//     }else if(a>b) {
-//         System.out.println("a is greater");
-//       }
-//       else{
-//         System.out.println("b is greater");
-//       }
-    
-//   }
-// }
-
-// import java.util.*;
-// public class FirstClass{
-//   public static void main(String args[]){
-//     Scanner sc = new Scanner(System.in);
-//     int button = sc.nextInt();
-
-  //   if(button==1){
-  //     System.out.println("hello");;
-  //   } else if(button==2){
-  //     System.out.println("nameste");
-  //   }else if(button==3){
-  //     System.out.println("salute");
-  //  }else{
-  //   System.out.println("invalid button");
-  //  }
-// switch(button){
-//   case 1:System.out.println("hello");
-//   break;
-//   case 2:System.out.println("namaste");
-//   break;
-//   case 3:System.out.println("salute");
-//   break;
-//   default : System.out.println("invalid");
-// }
-
-//   }
-// }
-// for loops
-
-// public class FirstClass{
-//   public static void main(String args[]){
-//     for(int i = 0 ; i < 11 ; i++){
-//     System.out.println(i);
-//     }
-
-//   }
-// }
-
-// while loop
-
-// public class FirstClass{
-//   public static void main(String args[]){
-//   int i = 0;
-//   while(i < 11) {
-//     System.out.println(i);
-//     i++;
-//   }  
-//   }
-// }
-
-
-//do while loop
-
-// public class FirstClass{
-//   public static void main(String args[]){
-//    int i=0;
-//     do{
-//       System.out.println(i);
-//       i++;
-//     }while(i<11);
-   
-//   }
-// }
-// import java.util.*;
-// ;public class FirstClass{
-//   public static void main(String args[]){
-//     Scanner sc = new Scanner(System.in);
-//     int n = sc.nextInt();
-//     int sum =0;
-//     for(int i = 0;i<=n; i++){
-//       sum=sum+i;
-//     }
-//     System.out.println(sum);
-//   }
-
-// }
-
-//  import java.util.*;
-//  public class FirstClass{
-//   public static void main(String args[]){
-//     Scanner sc = new Scanner(System.in);
-//     int n = sc.nextInt();
-
-//     for(int i = 1; i<=11;i++){
-//       System.out.println(n*i);
-//     }
-//   }
-//  }
-
-// import java.util.*;
-//   public class FirstClass{
-//     public static void main (String args[]){
-//       for (int i =1;i<=4;i++){
-//         System.out.println("****");
-//       }
-//     }
-//   }
-
-
-// import java.util.*;
-// public class FirstClass{
-//   public static void main(String args[]){
-//     int n = 4;
-//     int m= 5;
-//     // outer loop
-//     for(int i = 1;i<=n;i++){
-//       // innerloop
-//       for(int j =1;j<=m;j++){
-//         System.out.print("*");
-//       }
-//       System.out.println();
-//     }
-
-//   }
-//   }
 
 import java.util.*;
 public class FirstClass{
-  public static void main (String args[]){
-    int n=4;
-    int m = 5;
+  public static void main(String args[]){
+    int n = 4;
+    int m= 5;
     // outer loop
-   
+    for(int i = 1;i<=n;i++){
+      // innerloop
+      for(int j =1;j<=m;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
   }
-}
+  }
+
+
 
