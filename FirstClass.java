bf948465d9ac3acc744be89c14f89b728f7dbaab
Arus Rangra <arushrangra@gmail.com>
2026-02-16 22:46:19 +0530

    Update FirstClass.java

--- FirstClass.java
+++ FirstClass.java
@@ -27,4 +27,25 @@ public class FirstClass{
   }
 
 
+// find the factorial of a number
+import java.util.*;
+public class Functions{
+   public static void printfactorial(int n){
+      //loop
+      int factorial = 1;
+      for(int i=n;i>=1;i--){
+        factorial = factorial *i;
+      }
+      System.out.println(factorial);
+      return;
+   }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printfactorial(n);
+      
+    }
+
+}
 
