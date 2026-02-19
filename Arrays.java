f28e19b7387110bce76ee96f9bb3b06d64217f96
Arus Rangra <arushrangra@gmail.com>
2026-02-19 20:35:11 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,1 +1,16 @@
+//simple example first
+import java.util.*;
+public class Arrays {
+public static void main(String args[]){
+    int[] marks = new int[3];
+    marks[0] = 67;//phy
+    marks[1] = 70;//chem
+    marks[2] = 60;//math
+    System.out.println();
 
+    for(int i=0;i< marks.length;i++){
+        System.out.println(marks[i]);
+    }
+}
+    
+}
