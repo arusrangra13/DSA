58803b7ae2949c53f8b19b45d4a7be981ddc0404
Arus Rangra <arushrangra@gmail.com>
2026-02-19 21:43:33 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -32,3 +32,22 @@ public static void main(String args[]){
 }
     
 }
+
+//Giving input in arrays
+import java.util.*;
+public class Arrays {
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int size = sc.nextInt();
+    int numbers[] = new int[size];
+  //input
+   for(int i=0;i<size;i++){
+    numbers[i]=sc.nextInt();
+   }
+   //output
+    for(int i=0;i<size;i++){
+        System.out.println(numbers[i]);
+    }
+}
+    
+}
