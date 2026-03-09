d4301b8f8ff800b7526fe2f8df368d813458fc75
Arus Rangra <arushrangra@gmail.com>
2026-03-09 22:44:33 +0530

    Add String concatenation example in Strings class

---
+++ Stringconcatination.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Strings {
+    public static void main(String args[]){
+    //   concatination
+    String name = "Arus";
+    String lastname = "Rangra";
+    String fullname = name +" "+ lastname;
+    System.out.println( fullname);
+    }
+     
+}
