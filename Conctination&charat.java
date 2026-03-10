05e61a2417b10ce471e9d5a5c60fe87b7e1ed908
Arus Rangra <arushrangra@gmail.com>
2026-03-10 23:18:27 +0530

    Create Conctination&charat.java

---
+++ Conctination&charat.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Strings {
+    public static void main(String args[]){
+    //   concatination
+    String name = "Arus";
+    String lastname = "Rangra";
+    String fullname = name +" "+ lastname;
+    System.out.println( fullname.length());
+
+    // charat
+    for(int i =0; i<fullname.length();i++){
+        System.out.println(fullname.charAt(i));
+    }
+
+
+    }
+     
+}
