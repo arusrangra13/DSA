05b11c48514a55f25343259907c74b4eb1c08f9c
Arus Rangra <arushrangra@gmail.com>
2026-03-10 23:44:13 +0530

    Create compare two String.java

---
+++ compare two String.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String args[]) {
+
+        // compare
+        String name1 = "Arus";
+        String name2 = "Arus";
+
+        if(name1.compareTo(name2) == 0){
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+    }
+}
