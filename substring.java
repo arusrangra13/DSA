0827f8e455d3755842a473ae911269f4888b6825
Arus Rangra <arushrangra@gmail.com>
2026-03-14 23:22:43 +0530

    Create substring.java

---
+++ substring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String args[]) {
+        String sentence = "my name is Arus";
+        String name  = sentence.substring(8,sentence.length());
+        System.out.println(name);
+    }
+}
