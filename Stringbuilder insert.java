78c7e7ee3eaafe43f3368e56ed526f226260ebfe
Arus Rangra <arushrangra@gmail.com>
2026-03-16 22:08:12 +0530

    Create Stringbuilder insert.java

---
+++ Stringbuilder insert.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Stringbuilder {
+    public static void main(String args[]){
+    StringBuilder sb = new StringBuilder("Arus");
+    sb.insert(3,'t');
+    System.out.println(sb);
+    }
+}
