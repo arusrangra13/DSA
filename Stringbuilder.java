4d116e9de61cea741badd607d14646d62287a057
Arus Rangra <arushrangra@gmail.com>
2026-03-16 21:29:48 +0530

    Add Stringbuilder class with main method

---
+++ Stringbuilder.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Stringbuilder {
+    public static void main(String args[]){
+        StringBuilder sb = new StringBuilder("Arus");
+        System.out.println(sb);
+    }
+}
