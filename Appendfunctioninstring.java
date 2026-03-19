3e8ba971f646cbb6b0185b7a401e9c7ef5ba7e73
Arus Rangra <arushrangra@gmail.com>
2026-03-19 21:04:04 +0530

    Create Appendfunctioninstring.java

---
+++ Appendfunctioninstring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Stringbuilder {
+    public static void main(String args[]){
+    StringBuilder sb = new StringBuilder("Arus");
+    sb.append(" ");
+    sb.append("R");
+    sb.append("a");
+    sb.append("n");
+    sb.append("g");
+    sb.append("r");
+    sb.append("a");
+    System.out.println(sb);
+    }
+}
