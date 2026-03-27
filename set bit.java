7ccc5c460d2ae392d9f28b13cace4fa5c3a5c2d8
Arus Rangra <arushrangra@gmail.com>
2026-03-27 23:21:06 +0530

    Create set bit.java

---
+++ set bit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class operator{
+    public static void main(String args[]){
+        int n = 5;
+        int pos = 1 ;
+        int bitMask = 1<<pos;
+
+        int newnumber = bitMask | n ;
+        System.out.println(newnumber);
+
+
+    }      
+}
