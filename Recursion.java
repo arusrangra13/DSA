f262646844f4a09eed1e0976540174965e26bd3a
Arus Rangra <arushrangra@gmail.com>
2026-04-07 22:17:33 +0530

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,1 +1,14 @@
-
+//print numbers 5 to 1
+public class recursion {
+    public static void printnumb(int n ){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printnumb(n-1);
+    }
+    public static void main(String args[]){
+        int n = 5;
+        printnumb(n);
+    }
+}
