2375b9b308dcb130992b2977c86cc4ed4d34947e
Arus Rangra <arushrangra@gmail.com>
2026-04-12 09:57:07 +0530

    Create recursion2.java

---
+++ recursion2.java
@@ -0,0 +1,14 @@
+// print number from 1 to 5
+public class recursion{
+    public static void printnumb(int n){
+        if(n == 6){
+            return;
+        }
+        System.out.println(n);
+        printnumb(n+1);
+    }
+    public static void main(String args[]){
+        int n = 1;
+        printnumb(n);
+    }
+}
