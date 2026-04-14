420d1291c8a759ba4685d89bc21b40113de11c87
Arus Rangra <arushrangra@gmail.com>
2026-04-14 18:31:33 +0530

    Update recursion3.java

--- recursion3.java
+++ recursion3.java
@@ -1,1 +1,15 @@
-
+// print sum of the natural numbers
+public class recursion{
+    public static void printSum(int i,int n,int sum){
+        if(i==n){
+            sum+=1;
+            System.out.println(sum);
+            return; 
+        }
+          sum+=i;
+          printSum(i+1,n,sum);
+    }
+    public static void main(String args[]){
+        printSum(1,5,0);
+    }
+}
